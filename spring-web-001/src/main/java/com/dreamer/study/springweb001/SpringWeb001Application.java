d856c26105e71b1d1ff9f31b20be25cfe59e2835
zhangbaozhen@hh-medic.com <zhangbaozhen@hh-medic.com>
2019-11-05 19:02:51 +0800

    first commit

---
+++ spring-web-001/src/main/java/com/dreamer/study/springweb001/SpringWeb001Application.java
@@ -0,0 +1,16 @@
+package com.dreamer.study.springweb001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mac
+ */
+@SpringBootApplication
+public class SpringWeb001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeb001Application.class, args);
+    }
+
+}
